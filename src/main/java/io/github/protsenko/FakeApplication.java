6f9cbe8903b89d0e4bc61587d1ddf29bdad2ee15
Dmitry I. Protsenko <dprotsenko.repos@gmail.com>
2019-02-21 14:19:34 +0200

    Initial commit

---
+++ src/main/java/io/github/protsenko/FakeApplication.java
@@ -0,0 +1,11 @@
+package io.github.protsenko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FakeApplication.class, args);
+    }
+}
---
+++ src/test/java/io/github/protsenko/FakeApplicationTest.java
@@ -0,0 +1,8 @@
+package io.github.protsenko;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FakeApplicationTest {
+
+}
