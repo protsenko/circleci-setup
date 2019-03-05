5d7e05e9cca020829ce916ac5c6ca72a68f292aa
Dmitry I. Protsenko <dprotsenko.repos@gmail.com>
2019-03-05 16:29:37 +0200

    Add controller, and tests

--- src/test/java/io/github/protsenko/FakeApplicationTestIT.java
+++ src/test/java/io/github/protsenko/FakeApplicationTestIT.java
@@ -1,7 +1,14 @@
 package io.github.protsenko;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class FakeApplicationTestIT {
-
+    @Test
+    public void contextLoads() {
+    }
 }
