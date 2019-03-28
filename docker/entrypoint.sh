69860efe52b2fe1482df76fed7d959c1ca8bf85c
Dmitry I. Protsenko <dprotsenko.repos@gmail.com>
2019-03-28 17:36:26 +0200

    Add possibility run to docker image with -Xmx and -Xms

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,12 +2,32 @@
 
 set -Eeuo pipefail
 
+## Spring profile
 if [ -z ${SPRING_PROFILE+x} ]; then
     echo "SPRING_PROFILE environment variable is undefined";
     exit 1;
 fi
-APP_OPT="-Dspring.profiles.active=$SPRING_PROFILE"
 
+## -Xmx option
+MAX_MEM=""
+if [ -z ${MAX_MEMORY+x} ]; then
+    echo "MAX_MEMORY is undefined. Default value is 128m";
+    MAX_MEM="128m"
+else
+    MAX_MEM=$MAX_MEMORY
+fi
+
+## -Xms option
+MIN_MEM=""
+if [ -z ${MIN_MEMORY+x} ]; then
+    echo "MIN_MEMORY is undefined. Default value is 64m";
+    MIN_MEM="64m"
+else
+    MIN_MEM=$MIN_MEMORY
+fi
+
+
+APP_OPT="-Dspring.profiles.active=$SPRING_PROFILE -Xmx$MAX_MEM -Xms$MIN_MEM"
 RUN_APP="java ${APP_OPT} -jar /opt/app/app.jar"
 
 echo $RUN_APP
