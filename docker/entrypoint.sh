c76f29cbd001f33666e2086166fb79e86df8b2c4
Dmitry I. Protsenko <dprotsenko.repos@gmail.com>
2019-03-28 17:13:39 +0200

    Add possibility run to docker image with profile

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 
 set -Eeuo pipefail
-exec "$@"
+
+if [ -z ${SPRING_PROFILE+x} ]; then
+    echo "SPRING_PROFILE environment variable is undefined";
+    exit 1;
+fi
+APP_OPT="-Dspring.profiles.active=$SPRING_PROFILE"
+
+RUN_APP="java ${APP_OPT} -jar /opt/app/app.jar"
+
+echo $RUN_APP
+exec $RUN_APP
